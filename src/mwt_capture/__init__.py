21d3de4413e66bf3b719099fc0228768a7b5e1f6
Chung-Kuan Chen <b97b01045@gmail.com>
2024-05-23 11:44:10 +0900

    Add check framerate

--- src/mwt_capture/__init__.py
+++ src/mwt_capture/__init__.py
@@ -1,5 +1,7 @@
 import argparse
 import asyncio
+import ctypes
+import io
 from pathlib import Path
 from datetime import datetime
 import sys
@@ -227,6 +229,51 @@ def check_burst():
         print(ret)
     except Exception as e:
         print(e)
+
+
+def check_framerate():
+    try:
+        camera = Lucam(1)
+    except LucamError as e:
+        print(e)
+        return
+
+    properties = camera.default_snapshot()
+    # the parameter width 2592, height 1944, exposure 70.131, gain 0.375 from PC
+    # properties.ex
+    properties.exposure = 25
+    properties.gain = 0.2
+    properties.shutterType = API.LUCAM_SHUTTER_TYPE_ROLLING
+    print(properties)
+
+    # set camera to 8 bit VGA mode at low framerate
+    for rate in range(4, 17):
+        camera.SetFormat(
+            Lucam.FrameFormat(
+                0,
+                0,
+                2592,
+                1944,
+                API.LUCAM_PF_8,
+                binningX=1,
+                flagsX=1,
+                binningY=1,
+                flagsY=1,
+            ),
+            framerate=float(rate),
+        )
+
+        try:
+            for nframe in range(4, 17):
+                camera.StreamVideoControl("start_streaming")
+                t0 = time.monotonic()
+                video = camera.TakeVideo(nframe)  # take a 8 frames video
+                print(
+                    f"rate:{rate:f}, nframe:{nframe:d}, t: {(time.monotonic_ns() - t0)*1e-9:.3f} s"
+                )
+
+        finally:
+            camera.StreamVideoControl("stop_streaming")
 
 
 if __name__ == "__main__":
